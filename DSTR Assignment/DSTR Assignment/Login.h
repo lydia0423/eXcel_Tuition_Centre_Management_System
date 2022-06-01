396027099a4f4ab9c7faa50898684d86665cc022
sh1205653198 <103300952+sh1205653198@users.noreply.github.com>
2022-06-01 16:20:26 +0900

    Create Login.h

---
+++ DSTR Assignment/DSTR Assignment/Login.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Login{
+	string account, password;
+	int authority;
+
+	Login();
+	Login(string account, string password, int authority);
+};
