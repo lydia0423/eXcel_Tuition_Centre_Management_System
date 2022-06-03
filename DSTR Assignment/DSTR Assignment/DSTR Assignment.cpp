51a278a2a385e39c12244fdabb5a2440ab4336cf
leavewithwind <74440956+leavewithwind@users.noreply.github.com>
2022-06-03 10:38:54 +0800

    Update DSTR Assignment.cpp

--- DSTR Assignment/DSTR Assignment/DSTR Assignment.cpp
+++ DSTR Assignment/DSTR Assignment/DSTR Assignment.cpp
@@ -11,10 +11,18 @@ int main() {
 
 	while (login_loop == 0)
 	{
-		login_loop = adminMenu(tutorList, sizeOfTutorList);
-	}
-	
+		//此处写login识别功能
 
+		//if(登陆者为hr时)
+		//login_loop = hrMenu(tutorList, sizeOfTutorList);
+
+		//if(登陆者为admin时)
+		login_loop = adminMenu(tutorList, sizeOfTutorList);
+
+		//if(登陆者为tutor时)
+		//login_loop = tutorMenu(tutorList, sizeOfTutorList);
+	
+	}
 	
 	return 0;
 }
